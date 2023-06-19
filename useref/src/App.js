f3149be0cb956ad3cefa8e49fe72b84f0c04c692
Divyam Chauhan <mi.divyam@gmail.com>
2023-06-19 10:35:16 +0530

    useRef Practice

---
+++ useref/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import UseRef from "./Components/UseRef";
+function App() {
+  return (
+    <div className='App'>
+      <UseRef />
+    </div>
+  );
+}
+
+export default App;
---
+++ useref/src/Components/UseRef.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UseRef = () => {
+  return <h1>useRef</h1>;
+};
+
+export default UseRef;
